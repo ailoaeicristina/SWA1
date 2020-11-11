61acd3849ffaaa6d53cfb74865f2f1b630e1bf64
Cristina Ailoaei <ailoaeicristina@yahoo.com>
2020-11-11 15:14:49 +0100

    Added all tables

--- Assignment 3 - GUI Programming/weather_report_interactive_angular/static/components.js
+++ Assignment 3 - GUI Programming/weather_report_interactive_angular/static/components.js
@@ -1,57 +1,210 @@
 import model from './model.js'
 
-const module = angular.module('employeeApp', [])
+const module = angular.module('weatherReportApp', [])
 
-module.value('$model', { salary: 0, persons: [] })
+var d = new Date()
+d.setDate(d.getDate() - 5)
+module.value('$model', { historicals: [], predictions: [], place: 'All', startDate: d, endDate: new Date(), viewMode: 'historical' })
 
-module.component('employeeTable', {
+module.component('temperatureTable', {
   bindings: { attribute: '@' },
   template: `<table>
-    <thead><tr><td>Id</td><td>Name</td><td>Employee id</td><td>Salary</td><td>Manager</td></tr></thead>
-    <tbody id='employee_data'>
-        <tr ng-repeat="employee in $ctrl.model[$ctrl.attribute]">
-            <td>{{employee.id}}</td>
-            <td>{{employee.name}}</td>
-            <td ng-if='employee.employeeId'>{{employee.employeeId}}</td>
-            <td ng-if='employee.employeeId'>{{employee.salary}}</td>
-            <td ng-if='employee.employeeId'>{{employee.manager}}</td>
-            <td colspan='3' ng-if='!employee.employeeId'><button ng-click='$ctrl.hire(employee.id)'>Hire</button></td>
+    <thead><tr><td>Value</td><td>Unit</td><td>Time</td><td>Place</td></tr></thead>
+    <tbody id='temperature_data'>
+        <tr ng-repeat="temperature in $ctrl.model[$ctrl.attribute]">
+            <td>{{temperature.value}}</td>
+            <td>{{temperature.unit}}</td>
+            <td>{{temperature.time}}</td>
+            <td>{{temperature.place}}</td>
         </tr>
     </tbody>
   </table>`,
   controller: ['$model', '$scope', function ($model, $scope) {
     this.model = $model
-    // The parent of the component scope is the controller scope
-    this.hire = $scope.$parent.hire 
-  }]            
+    console.log("Look here", $model)
+  }]
 })
 
-module.controller('EmployeeController', function($scope, $model, $http) {
-  $scope.model = $model
-  let aModel
-  $http.get('http://localhost:9090/persons')
-  .then(({data: persons}) => {
-    $http.get('http://localhost:9090/employees')
-    .then(({data: employees}) => {
-      aModel = model(persons, employees)
-      $scope.model.persons = aModel.personData()
-    })
-  })
-  .catch(console.err)
+module.component('precipitationTable', {
+  bindings: { attribute: '@' },
+  template: `<table>
+    <thead><tr><td>Total</td><td>Unit</td><td>Place</td></tr></thead>
+    <tbody id='precipitation_data'>
+        <tr ng-repeat="precipitation_data in $ctrl.model[$ctrl.attribute]">
+            <td>{{precipitation_data.total}}</td>
+            <td>{{precipitation_data.unit}}</td>
+            <td>{{precipitation_data.place}}</td>
+        </tr>
+    </tbody>
+  </table>`,
+  controller: ['$model', '$scope', function ($model, $scope) {
+    this.model = $model
+    console.log("Look here", $model)
+  }]
+})
 
-  $scope.hire = id => {
-    if ($scope.model.salary > 0) {
-      const headers = { 'Content-Type': 'application/json', Accept: 'application/json' }
-      $http.post('http://localhost:9090/employees', JSON.stringify({salary: $scope.model.salary, manager: false}), { headers })
-      .then(({data: employee})=> {
-        $http.patch('http://localhost:9090/persons/' + id, JSON.stringify({ employeeId: employee.employeeId }), {headers })
-        .then(({data: person}) => {
-          aModel.addEmployee(employee)
-          aModel.updatePerson(person)
-          $scope.model.persons = aModel.personData()
-          $scope.model.salary = 0
+module.component('windspeedTable', {
+  bindings: { attribute: '@' },
+  template: `<table>
+    <thead><tr><td>Average</td><td>Unit</td><td>Place</td></tr></thead>
+    <tbody id='windspeed_data'>
+        <tr ng-repeat="windspeed in $ctrl.model[$ctrl.attribute]">
+            <td>{{windspeed.average}}</td>
+            <td>{{windspeed.unit}}</td>
+            <td>{{windspeed.place}}</td>
+        </tr>
+    </tbody>
+  </table>`,
+  controller: ['$model', '$scope', function ($model, $scope) {
+    this.model = $model
+    console.log("Look here", $model)
+  }]
+})
+
+module.component('winddirectionTable', {
+  bindings: { attribute: '@' },
+  template: `<table>
+    <thead><tr><td>Direction</td><td>Place</td></tr></thead>
+    <tbody id='winddirection_data'>
+        <tr ng-repeat="winddirection in $ctrl.model[$ctrl.attribute]">
+            <td>{{winddirection.direction}}</td>
+            <td>{{winddirection.place}}</td>
+        </tr>
+    </tbody>
+  </table>`,
+  controller: ['$model', '$scope', function ($model, $scope) {
+    this.model = $model
+    console.log("Look here", $model)
+  }]
+})
+
+module.component('cloudcoverageTable', {
+  bindings: { attribute: '@' },
+  template: `<table>
+    <thead><tr><td>Average</td><td>Unit</td><td>Place</td></tr></thead>
+    <tbody id='cloudcoverage_data'>
+        <tr ng-repeat="temperature in $ctrl.model[$ctrl.attribute]">
+            <td>{{cloudcoverage.average}}</td>
+            <td>{{cloudcoverage.unit}}</td>
+            <td>{{cloudcoverage.place}}</td>
+        </tr>
+    </tbody>
+  </table>`,
+  controller: ['$model', '$scope', function ($model, $scope) {
+    this.model = $model
+    console.log("Look here", $model)
+  }]
+})
+
+module.component('temperaturepredictionsTable', {
+  bindings: { attribute: '@' },
+  template: `<table>
+    <thead><tr><td>From</td><td>To</td><td>Type</td><td>Unit</td><td>Time</td><td>Place</td></tr></thead>
+    <tbody id='temperaturepredictions_data'>
+        <tr ng-repeat="temperaturepredictions in $ctrl.model[$ctrl.attribute]">
+            <td>{{temperaturepredictions.from}}</td>
+            <td>{{temperaturepredictions.to}}</td>
+            <td>{{temperaturepredictions.type}}</td>
+            <td>{{temperaturepredictions.unit}}</td>
+            <td>{{temperaturepredictions.time}}</td>
+            <td>{{temperaturepredictions.place}}</td>
+        </tr>
+    </tbody>
+  </table>`,
+  controller: ['$model', '$scope', function ($model, $scope) {
+    this.model = $model
+    console.log("Look here", $model)
+  }]
+})
+
+module.component('precipitationpredictionsTable', {
+  bindings: { attribute: '@' },
+  template: `<table>
+    <thead><tr><td>From</td><td>To</td><td>Precipitation types</td><td>Type</td><td>Unit</td><td>Time</td><td>Place</td></tr></thead>
+    <tbody id='precipitationpredictions_data'>
+        <tr ng-repeat="precipitationpredictions in $ctrl.model[$ctrl.attribute]">
+            <td>{{precipitationpredictions.from}}</td>
+            <td>{{precipitationpredictions.to}}</td>
+            <td>{{precipitationpredictions.precipitation_types}}</td>
+            <td>{{precipitationpredictions.type}}</td>
+            <td>{{precipitationpredictions.unit}}</td>
+            <td>{{precipitationpredictions.time}}</td>
+            <td>{{precipitationpredictions.place}}</td>
+        </tr>
+    </tbody>
+  </table>`,
+  controller: ['$model', '$scope', function ($model, $scope) {
+    this.model = $model
+    console.log("Look here", $model)
+  }]
+})
+
+module.component('windspeedpredictionsTable', {
+  bindings: { attribute: '@' },
+  template: `<table>
+    <thead><tr><td>From</td><td>To</td><td>Directions</td><td>Type</td><td>Unit</td><td>Time</td><td>Place</td></tr></thead>
+    <tbody id='windspeedpredictions_data'>
+        <tr ng-repeat="windspeedpredictions in $ctrl.model[$ctrl.attribute]">
+            <td>{{windspeedpredictions.from}}</td>
+            <td>{{windspeedpredictions.to}}</td>
+            <td>{{windspeedpredictions.directions}}</td>
+            <td>{{windspeedpredictions.type}}</td>
+            <td>{{windspeedpredictions.unit}}</td>
+            <td>{{windspeedpredictions.time}}</td>
+            <td>{{windspeedpredictions.place}}</td>
+        </tr>
+    </tbody>
+  </table>`,
+  controller: ['$model', '$scope', function ($model, $scope) {
+    this.model = $model
+    console.log("Look here", $model)
+  }]
+})
+
+module.component('cloudcoveragepredictionsTable', {
+  bindings: { attribute: '@' },
+  template: `<table>
+    <thead><tr><td>From</td><td>To</td><td>Type</td><td>Unit</td><td>Time</td><td>Place</td></tr></thead>
+    <tbody id='cloudcoveragepredictions_data'>
+        <tr ng-repeat="cloudcoveragepredictions in $ctrl.model[$ctrl.attribute]">
+            <td>{{cloudcoveragepredictions.from}}</td>
+            <td>{{cloudcoveragepredictions.to}}</td>
+            <td>{{cloudcoveragepredictions.type}}</td>
+            <td>{{cloudcoveragepredictions.unit}}</td>
+            <td>{{cloudcoveragepredictions.time}}</td>
+            <td>{{cloudcoveragepredictions.place}}</td>
+        </tr>
+    </tbody>
+  </table>`,
+  controller: ['$model', '$scope', function ($model, $scope) {
+    this.model = $model
+    console.log("Look here", $model)
+  }]
+})
+
+module.controller('WeatherReportController', function ($scope, $model, $http) {
+  $scope.model = $model
+  console.log($scope.model)
+  let aModel
+  $http.get('http://localhost:8080/data')
+    .then(({ data: data }) => {
+      $http.get('http://localhost:8080/forecast')
+        .then(({ data: forecast }) => {
+          aModel = model(data, forecast, $scope.model.place, $scope.model.startDate, $scope.model.endDate, $scope.model.viewMode)
+          $scope.model.historicals = aModel.historicals
+          $scope.model.predictions = aModel.predictions
+          $scope.model.minTemperatures = aModel.minTemperatures
+          $scope.model.maxTemperatures = aModel.maxTemperatures
+          $scope.model.totalPrecipitations = aModel.totalPrecipitations
+          $scope.model.avgWindSpeed = aModel.avgWindSpeed
+          $scope.model.dominantWindDirection = aModel.dominantWindDirection
+          $scope.model.avgCloudCoverage = aModel.avgCloudCoverage
+          $scope.model.temperaturePredictions = aModel.temperaturePredictions
+          $scope.model.precipitationPredictions = aModel.precipitationPredictions
+          $scope.model.windspeedPredictions = aModel.windspeedPredictions
+          $scope.model.cloudcoveragePredictions = aModel.cloudcoveragePredictions
         })
-      })
-    }
-  }
+    })
+    .catch(console.err)
+  console.log($scope.model)
 })
--- Assignment 3 - GUI Programming/weather_report_interactive_angular/static/model.js
+++ Assignment 3 - GUI Programming/weather_report_interactive_angular/static/model.js
@@ -1,20 +1,99 @@
-const model = (persons, employees) => {
-    const employeeMap = {}
-    employees.forEach(e => employeeMap[e.employeeId] = e)
+const model = (historicals, predictions, place, startDate, endDate, viewMode) => {
+    console.log("---------- model output")
+    let records = viewMode === 'historical' ? historicals : predictions
+    console.log(place)
+    console.log(startDate)
+    console.log(endDate)
+    console.log(viewMode)
+    console.log(records)
+    console.log("---------- model output end")
+    const places = place === 'All' ? ['Horsens', 'Aarhus', 'Copenhagen'] : [place]
+    records = records.filter(r => place === 'All' || r.place === place)
+    records = records.filter(r => Date.parse(r.time) >= startDate.getTime())
+    records = records.filter(r => Date.parse(r.time) <= endDate.getTime())
 
-    const personData = () => persons
-        .map(p => ({ ...p, ...employeeMap[p.employeeId]}))
+    const addHistoricalRecord = newRecord => { historicals.concat(newRecord) }
+    const refreshPredictions = refreshPredictions => { predictions = refreshPredictions }
 
-    const updatePerson = p => {
-        let i = persons.findIndex(pp => pp.id == p.id)
-        persons[i] = p
+    //#region get historical data
+    // Minimum temperatures for the date interval
+    let minTemperatures = []
+    let minTemp
+    places.forEach(p => {
+        minTemp = records.filter(r => r.place === p).filter(r => r.type === "temperature").reduce((acc, v) => acc.value < v.value ? acc : v, { value: null })
+        if (minTemp !== null)
+            minTemperatures.push(minTemp)
+    });
+
+    // Maximum temperatures for the date interval
+    let maxTemperatures = []
+    let maxTemp
+    places.forEach(p => {
+        maxTemp = records.filter(r => r.place === p).filter(r => r.type === "temperature").reduce((acc, v) => acc.value > v.value ? acc : v, { value: null })
+        if (maxTemp !== null)
+            maxTemperatures.push(maxTemp)
+    });
+
+    // Total precipitation for the date interval
+    let totalPrecipitations = []
+    let totalPrecip, totalPrecipForCity, totalPrecipFirstObj
+    places.forEach(p => {
+        totalPrecipForCity = records.filter(r => r.place === p).filter(r => r.type === "precipitation")
+        totalPrecipFirstObj = totalPrecipForCity[0]
+        totalPrecip = totalPrecipForCity.reduce((acc, v) => acc + v.value, 0)
+        if (totalPrecip !== 0)
+            totalPrecipitations.push({ total: totalPrecip, unit: totalPrecipFirstObj.unit, place: p, })
+    });
+
+    // Average wind speed for the date interval
+    let avgWindSpeed = []
+    let avgWS, avgWSForCity, avgWSFirstObj
+    places.forEach(p => {
+        avgWSForCity = records.filter(r => r.place === p).filter(r => r.type === "wind speed")
+        if (avgWSForCity.length === 0)
+            return
+        avgWSFirstObj = avgWSForCity[0]
+        avgWS = avgWSForCity.reduce((acc, v) => acc + v.value, 0) / avgWSForCity.length
+        if (avgWS !== 0)
+            avgWindSpeed.push({ average: avgWS, unit: avgWSFirstObj.unit, place: p })
+    });
+
+    // Dominant wind direction for the date interval
+    let dominantWindDirection = []
+    let dominantWD
+    places.forEach(p => {
+        dominantWD = records.filter(r => r.place === p).filter(r => r.type === "wind speed").map(d => d.direction).reduce(
+            (acc, v, i, arr) => (arr.filter(val => val === acc).length >= arr.filter(val => val === v).length ? acc : v), null)
+        if (dominantWD !== null)
+            dominantWindDirection.push({ direction: dominantWD, place: p })
+    });
+
+    // Average wind speed for the date interval
+    let avgCloudCoverage = []
+    let avgCC, avgCCForCity, avgCCFirstObj
+    places.forEach(p => {
+        avgCCForCity = records.filter(r => r.place === p).filter(r => r.type === "cloud coverage")
+        if (avgCCForCity.length === 0)
+            return
+        avgCCFirstObj = avgCCForCity[0]
+        avgCC = avgCCForCity.reduce((acc, v) => acc + v.value, 0) / avgCCForCity.length
+        if (avgCC !== 0)
+            avgCloudCoverage.push({ average: avgCC, unit: avgCCFirstObj.unit, place: p })
+    });
+    //#endregion
+
+    //#region get predictive data
+    const temperaturePredictions = records.filter(r => r.type === "temperature")
+    const precipitationPredictions = records.filter(r => r.type === "precipitation")
+    const windSpeedPredictions = records.filter(r => r.type === "wind speed")
+    const cloudCoveragePredictions = records.filter(r => r.type === "cloud coverage")
+    //#endregion
+
+    return {
+        historicals, predictions, place, startDate, endDate, viewMode, addHistoricalRecord, refreshPredictions,
+        minTemperatures, maxTemperatures, totalPrecipitations, avgWindSpeed, dominantWindDirection, avgCloudCoverage,
+        temperaturePredictions, precipitationPredictions, windSpeedPredictions, cloudCoveragePredictions
     }
-    const addEmployee = e => {
-        employees.push(e)
-        employeeMap[e.employeeId] = e
-    }
-
-    return { personData, updatePerson, addEmployee }
 }
 
 export default model
